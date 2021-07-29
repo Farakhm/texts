74c7f4dd35d00f69b11c87b4383336f427d4cff3
Wim Winter <farakhm@yandex.ru>
2021-07-29 19:09:42 +0500

    index commit

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from service import text_metrix
+
+# Открываем текст
+f = open('first.txt', 'r')
+g = f.read()
+f.close()
+
+# Снимаем различные метрики текста
+
+
+
 
